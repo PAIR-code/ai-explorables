8105697b607cea52640e08b9a1101fb5558b88f4
Adam Pearce <adampearce@google.com>
2021-08-09 21:33:32 -0400

    starts scatter

---
+++ server-side/fill-in-the-blank/scatter-plot-colab/two-sentences/script.js
@@ -0,0 +1,12 @@
+console.clear()
+d3.select('body').selectAppend('div.tooltip.tooltip-hidden')
+
+
+console.log(window.python_data)
+console.log(window.python_settings)
+
+
+var sel = d3.select('.container').html('')
+
+
+
---
+++ server-side/fill-in-the-blank/scatter-plot-colab/two-sentences/watch-files.js
@@ -0,0 +1,57 @@
+!(function(){
+  function watchFile(path){
+    var lastStr = ''
+
+    console.log(path)
+    function check(){
+      d3.text(path + '?' + Math.random(), (err, nextStr) => {
+        if (err){
+          console.log(err)
+          return check()
+        }
+
+        if (nextStr == lastStr) return
+        lastStr = nextStr
+
+        if (path.includes('.js')){
+          console.log('js', new Date())
+          Function(nextStr.replace('\n', ';').replace('\n', ';'))()
+        }
+
+        if (path.includes('.css')){
+          console.log('css', new Date())
+
+          Array.from(document.querySelectorAll('link'))
+            .filter(d => d.href.includes(path) || d.href.includes('__hs_placeholder'))
+            .forEach(d => d.href = path + '?' + Math.random())
+        }
+      })
+
+      if (python_settings.isDev) setTimeout(check, 100)
+    }
+    check()
+  }
+
+  ;['style.css', 'script.js'].forEach(filename => {
+    var root = document.currentScript.src.replace('watch-files.js', '').split('?')[0]
+    var path = root + filename
+
+    if (python_settings.isDev){
+      watchFile(path)
+    } else {
+      if (path.includes('.js')){
+        var node = document.createElement('script')
+        node.setAttribute('src', path)
+        document.body.appendChild(node)
+      }
+
+      if (path.includes('.css')){
+        Array.from(document.querySelectorAll('link'))
+          .filter(d => d.href.includes(path) || d.href.includes('__hs_placeholder'))
+          .forEach(d => d.href = path + '?' + Math.random())
+      }
+    }
+  })
+})()
+
+
