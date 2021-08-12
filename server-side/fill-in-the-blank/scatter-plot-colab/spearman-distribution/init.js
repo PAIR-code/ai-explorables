72ac99cf9759b6570085655bb31b20afb55afa76
Adam Pearce <adampearce@google.com>
2021-08-12 00:04:06 -0400

    highlight active

--- server-side/fill-in-the-blank/scatter-plot-colab/spearman-distribution/init.js
+++ server-side/fill-in-the-blank/scatter-plot-colab/spearman-distribution/init.js
@@ -28,48 +28,80 @@ window.init = function(){
     sent.sentenceIndex = +sent.key
     sent.s0 = python_data.sentences[sent.sentenceIndex].s0
     sent.s1 = python_data.sentences[sent.sentenceIndex].s1
+    sent.orig = python_data.sentences[sent.sentenceIndex].orig
 
-    sent.corr = ss.sampleCorrelation(sent.map(d => d.i0), sent.map(d => d.i1))
-    // sent.corr = ss.sampleCorrelation(sent.map(d => d.e0), sent.map(d => d.e1))
+    sent.corrA = ss.sampleCorrelation(sent.map(d => d.i0), sent.map(d => d.i1))
+    // sent.corrA = ss.sampleCorrelation(sent.map(d => d.e0), sent.map(d => d.e1))
   })
 
   var sel = d3.select('.container').html(`
-    <div class='beeswarm'></div>
-    <div class='pair'></div>
+    <div class='left'>
+      <div class='beeswarm'></div>
+      <div class='pair'></div>
+    </div>
+    <div class='right'>
+      <div class='list'></div>
+    </div>
   `)
+    .st({width: 1100})
+  d3.selectAll('.left,.right').st({width: 500, display: 'inline-block', verticalAlign: 'top'})
 
   function initBeeswarm(bySentence, sel){
-    console.log(sel.node())
     var c = d3.conventions({
       sel: sel.append('div'),
       height: 100,
-      totalWidth: 500,
-      margin: {left: 100}
+      totalWidth: 400,
+      margin: {left: 0}
     })
 
-    c.x.domain(d3.extent(bySentence.map(d => +d.corr)))
+    c.x.domain(d3.extent(bySentence.map(d => +d.corrA))).nice()
     // c.x.domain([0, 1])
+    c.xAxis.ticks(5)
     d3.drawAxis(c)
     c.svg.select('.y').remove()
     c.svg.selectAll('.tick').st({display: 'block'})
 
     var simulation = d3.forceSimulation(bySentence)
-      .force("x", d3.forceX(d => c.x(d.corr)).strength(1))
+      .force("x", d3.forceX(d => c.x(d.corrA)).strength(1))
       .force("y", d3.forceY(c.height / 2))
       .force("collide", d3.forceCollide(4))
       .stop()
 
-    for (var i = 0; i < 120; ++i) simulation.tick();
+    for (var i = 0; i < 120; ++i) simulation.tick()
 
-    c.svg.appendMany('circle', bySentence)
+    c.svg.append('text').text('text')
+      .text('Distribution of Spearman Correlation Coefficients')
+      .at({dy: 10, fontWeight: 600})
+
+    c.svg.appendMany('circle.sentence', bySentence)
       .translate(d => [d.x, d.y])
       .at({
-        r: 3
+        r: 3,
+        fill: 'none',
+        stroke: '#000'
       })
       .on('mouseover', setSentenceAsPair)
-
   }
   initBeeswarm(bySentence, d3.select('.beeswarm'))
+
+
+  function initList(bySentence, sel){
+    var sentenceSel = sel.st({height: 500, overflowY: 'scroll', cursor: 'default'})
+      .appendMany('div.sentence', _.sortBy(bySentence, d => d.corrA))
+      .on('mouseover', setSentenceAsPair)
+      .st({padding: 2})
+
+    sentenceSel.append('span')
+      .text(d => (d3.format('+.2f')(d.corrA)).replace('0.', '.'))
+      .st({marginRight: 10, color: '#aaa'})
+
+    sentenceSel.append('span')
+      .text(d => d.orig.replace('[', '').replace(']', ''))
+
+
+  }
+  initList(bySentence, d3.select('.list'))
+
 
 
   function setSentenceAsPair(s){
@@ -89,7 +121,16 @@ window.init = function(){
     var sel = d3.select('.pair').html('').st({width: 400})
 
     initPair(s, sel)
+
+    d3.selectAll('.sentence').classed('active', d => d == s)
+
+    d3.selectAll('div.sentence').filter(d => d == s)
+      .each(function(){
+        this.scrollIntoView({ block: 'nearest', inline: 'start' })
+      })
   }
+
+  setSentenceAsPair(bySentence[0])
 
 }
 
